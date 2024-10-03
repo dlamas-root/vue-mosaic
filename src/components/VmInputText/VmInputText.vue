d8365da8b8cc9b0e1e15afa515b4661f91c1b777
Daniel Lamas <dlamas.15040@gmail.com>
2024-10-03 10:53:24 -0300

    Extract common content to new component

--- src/components/VmInputText/VmInputText.vue
+++ src/components/VmInputText/VmInputText.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, ref, type ModelRef, type Ref } from 'vue'
 import VmIcon from '../VmIcon/VmIcon.vue'
+import VmField from '../VmField/VmField.vue'
 
 defineOptions({
   name: 'VmInputText'
@@ -41,38 +42,37 @@ const count = computed(() => model.value?.length || 0)
 </script>
 
 <template>
-  <div class="vm-field">
-    <!-- TODO: prepend text fix -->
-    <div class="vm-input-field" :class="required && !model ? 'vm-input-required' : 'vm-input-not-required'">
-      <span v-if="prefix" class="vm-input-prefix">
-        {{ prefix }}
-      </span>
-      <slot name="prepend">
-        <VmIcon v-if="prependIcon" :name="prependIcon" :color="iconColor" />
-      </slot>
-      <input
-        v-model="model"
-        :id
-        :name
-        :disabled
-        :readonly
-        :required
-        :type="inputType"
-        :maxlength="maxLength"
-        :class="{ 'pl-5': prependIcon }"
-      />
-      <label :style="{ paddingLeft: (prependIcon || prefix) && '1.5rem' }">
-        <slot name="label">
-          {{ label }}
-        </slot>
-      </label>
-      <span class="vm-input-suffix" v-if="suffix">
-        {{ suffix }}
-      </span>
+  <VmField
+    :appendIcon
+    :prependIcon
+    :counter
+    :counterActual="count"
+    :counterMax="maxLength"
+    :hint
+    :label
+    :loading
+    :prefix
+    :suffix
+    :iconColor
+  >
+    <input
+      v-model="model"
+      :id
+      :name
+      :disabled
+      :readonly
+      :required
+      :type="inputType"
+      :maxlength="maxLength"
+      :class="{ 'pl-5': prependIcon }"
+    />
+    <template #prepend>
+      <slot name="prepend" />
+    </template>
+    <template #append>
       <slot name="append">
-        <VmIcon v-if="appendIcon" :name="appendIcon" :color="iconColor" />
         <span
-          v-else-if="type === 'password'"
+          v-if="!appendIcon && type === 'password'"
           class="vm-icon-field"
           @click="showPassword = !showPassword"
         >
@@ -98,157 +98,9 @@ const count = computed(() => model.value?.length || 0)
           </Transition>
         </span>
       </slot>
-    </div>
-    <div class="vm-hint-field">
-      <Transition name="fade" mode="out-in">
-        <span v-if="hint" class="vm-hint-message">{{ hint }}</span>
-      </Transition>
-      <div
-        v-if="counter"
-        class="vm-hint-counter"
-        :class="count == maxLength ? 'vm-counter-limit' : ''"
-      >
-        {{ count }}<span v-if="maxLength">/{{ maxLength }}</span>
-      </div>
-    </div>
-  </div>
+    </template>
+    <template #label>
+      <slot name="label" />
+    </template>
+  </VmField>
 </template>
-
-<style lang="scss">
-.vm-field {
-  .vm-input-field {
-    position: relative;
-    margin: 15px 0 0 0;
-    transition: 0.15s ease;
-    display: flex;
-    flex-direction: row;
-
-    label {
-      position: absolute;
-      top: 52%;
-      left: 2px;
-      transform: translateY(-50%);
-      color: #703a3a;
-      font-size: 14px;
-      pointer-events: none;
-      transition: 0.15s ease;
-
-      &.vm-label-focused {
-        font-size: 0.7rem;
-        top: 10px;
-        transform: translateY(-120%);
-      }
-    }
-
-    &:has(> input:focus, input:valid) {
-      border-bottom: 2px solid #ac3f3f;
-    }
-
-    input {
-      width: 100%;
-      height: 30px;
-      background: transparent;
-      border: none;
-      outline: none;
-      font-size: 16px;
-      color: #000;
-
-      &:focus,
-      &:valid {
-        ~ label {
-          font-size: 0.7rem;
-          top: 10px;
-          transform: translateY(-120%);
-        }
-      }
-
-      &:required ~ label::after {
-        content: ' *';
-      }
-    }
-    span.vm-input-prefix {
-      align-self: center;
-      margin: 0 5px 0 0;
-    }
-    span.vm-input-suffix {
-      align-self: center;
-      margin: 0 5px;
-    }
-    span.vm-icon-field {
-      margin-top: 3px;
-      margin-right: 5px;
-      cursor: pointer;
-    }
-  }
-  .vm-input-not-required{
-    border-bottom: 2px solid gainsboro;
-  }
-
-  .vm-input-required {
-    border-bottom: 2.5px solid #d70000 ;
-  }
-  .vm-hint-field {
-    display: flex;
-    flex-direction: row;
-    font-size: 12px;
-    justify-content: space-between;
-
-    .vm-hint-message {
-      display: inline-block;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      color: #856262;
-    }
-    .vm-hint-counter {
-      margin-left: auto;
-      margin-top: 2px;
-      color: #703a3a;
-    }
-
-    .vm-counter-limit {
-      color: #d23232;
-      font-weight: 600;
-    }
-  }
-
-  /* we will explain what these classes do next! */
-  .fade-in-top {
-    animation: FadeInTop 0.6s ease-in-out 0s 1 normal both;
-  }
-
-  @keyframes FadeInTop {
-    0% {
-      opacity: 0;
-      transform: translateY(-50px);
-    }
-
-    100% {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
-
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.3s ease;
-  }
-
-  .fade-enter-from,
-  .fade-leave-to {
-    opacity: 0;
-  }
-
-  .zoom-fade-in-enter-active,
-  .zoom-fade-in-leave-active {
-    transition: all 0.3s ease-in-out;
-    scale: 1.1;
-  }
-
-  .zoom-fade-in-enter-from,
-  .zoom-fade-in-leave-to {
-    opacity: 0;
-    scale: 1;
-  }
-}
-</style>
